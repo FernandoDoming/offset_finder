4b1b5d0b71100fea17f127683a58533ef0e06fe9
Fernando Domínguez <fdominguez@redborder.net>
2016-08-10 10:08:49 +0200

    Refactor functions & add params

--- bintools/splitter.py
+++ bintools/splitter.py
@@ -1,7 +1,7 @@
 import os
 
 # Splits a file using the dsplit mechanism
-def dsplit(fromfile, todir, chunksize = 1024):
+def dsplit(fromfile, todir = os.getcwd(), offset = 0, limit = None, chunksize = 1024):
     if not os.path.exists(todir):                  # caller handles errors
         os.mkdir(todir)                            # make dir, read/write parts
 
@@ -11,21 +11,23 @@ def dsplit(fromfile, todir, chunksize = 1024):
     partnum = 0
     while cont:
         if chunksize > filesize:
+            # Do 1 more read if chunksize > filesize
             cont = False
             chunksize = filesize
-        chunk = __read_write_block(fromfile, chunksize)
-        if not chunk: break
         partnum  = partnum + 1
-        filename = os.path.join(todir, ('%s.part%d' % (original_file, partnum)))
-        fileobj  = open(filename, 'wb')
-        fileobj.write(chunk)
-        fileobj.close()
+        tofile = os.path.join(todir, ('%s.part%d' % (original_file, partnum)))
+        chunk = __read_write_block(fromfile, chunksize, tofile)
         chunksize *= 2
 
 #### Private methods
 
-def __read_write_block(f, n):
-    stream = open(f, 'rb')
+def __read_write_block(fromfile, n, tofile, offset = 0):
+    stream = open(fromfile, 'rb')
     chunk = stream.read(n)
     stream.close()
-    return chunk
+    if not chunk: return
+
+    fileobj = open(tofile, 'wb')
+    fileobj.write(chunk)
+    fileobj.close()
+    return fileobj
