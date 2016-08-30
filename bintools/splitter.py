9dcbe4688e0509e60b28a2de99a94f569b3307f4
Fernando Domínguez <fdominguez@redborder.net>
2016-08-30 11:02:16 +0200

    Iterate over parts

--- bintools/splitter.py
+++ bintools/splitter.py
@@ -6,7 +6,7 @@ from colors import colors
 
 BUFFER = 1024
 
-def dsplit(fromfile, todir=os.getcwd(), offset=0, limit=None, chunksize=1024, fill=False, verbose=False):
+def dsplit(fromfile, todir=os.getcwd(), offset=0, limit=None, chunksize=1024, fill=False):
     """
     Splits a file using the dsplit mechanism
     """
@@ -15,11 +15,18 @@ def dsplit(fromfile, todir=os.getcwd(), offset=0, limit=None, chunksize=1024, fi
 
     original_file = os.path.basename(fromfile)
     filesize = os.path.getsize(fromfile)
+
+    if fill:
+        # do not truncate headers if fill is set
+        if limit is not None: limit = offset + limit;
+        offset = 0
+
+    # calculate sizes to operate with
     if limit is not None: to_divide = min(limit, filesize - offset)
     else: to_divide = filesize - offset
 
-    if verbose:     logging.info("%sdsplit: %s%d parts will be generated in %s" %
-                    (colors.OKGREEN, colors.ENDC, math.ceil(to_divide / chunksize), todir))
+    logging.info("%sdsplit: %s%d parts will be generated in %s" %
+        (colors.OKGREEN, colors.ENDC, math.ceil(to_divide / chunksize), todir))
 
     cont = True; partnum = 0; read = chunksize
     while cont:
--- find.py
+++ find.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import shutil
 import argparse
 import logging
 import re
@@ -10,12 +11,35 @@ import multiav.core
 from bintools import splitter
 from colors import colors
 
+################ Functions ################
+
+def scan_parts(parts):
+  detected_parts = []
+  parts_pattern = re.compile('.+part(\d+)$')
+
+  for part, result in parts.iteritems():
+    if result[0] != 'FOUND': continue
+
+    matched = parts_pattern.match(part)
+    part_n = int(matched.group(1))
+    detected_parts.append(part_n)
+    logging.info("Part %d detected as %s" % (part_n, result[1]))
+
+  return detected_parts
+
+################   main   ################
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("file", help="the file to analyze")
   parser.add_argument("-s", "--step", type=int, help="initial step size for dsplit. (default 1Kb)")
-  parser.add_argument("-p", "--precision", type=int, help="Precision for the signature location (absolute error in bytes). (default 100)")
+  parser.add_argument("-i", "--iter", type=int, help="Maximum iterations. (default None)")
+  parser.add_argument("-p", "--precision", type=int, help="Precision for the signature location " +
+                                                          "(absolute error in bytes). (default 100)")
   parser.add_argument("-v", "--verbose", action='store_true', help="Verbose output")
+  parser.add_argument("-t", "--truncate", action='store_true', help="Truncate instead of filling with zeros. " +
+                                                                    "Filling with zeros usually works better " +
+                                                                    "but uses more space")
   args = parser.parse_args()
 
   if args.verbose: log_level = logging.INFO
@@ -24,30 +48,49 @@ if __name__ == "__main__":
   logging.basicConfig(level=log_level,
                       format='%(levelname)s: %(message)s')
 
-  parts_pattern = re.compile('.+part(\d+)$')
+  base_dir      = os.path.dirname(os.path.realpath(__file__))
+  multi_av      = multiav.core.CMultiAV( os.path.join(base_dir, 'multiav.cfg') )
 
-  step = args.step or 1024
-  precision = args.precision or 100
+  step       = args.step or 1024
+  precision  = args.precision or 100
   dsplit_dir = os.path.join(os.getcwd(), 'offsets')
-  splitter.dsplit(args.file, todir=dsplit_dir, chunksize=step, verbose=args.verbose)
+  max_iter   = args.iter or float('inf')
 
-  base_dir = os.path.dirname(os.path.realpath(__file__))
-  multi_av = multiav.core.CMultiAV( os.path.join(base_dir, 'multiav.cfg') )
-  logging.info("Beginning scanning at %s..." % (dsplit_dir))
-  scans = multi_av.scan(dsplit_dir, multiav.core.AV_SPEED_MEDIUM)
+  offset = 0; limit = None; i = 0
+  while step > precision:
+    logging.info("Beginning iteration %d. Offset: %d, step: %d" % (i, offset, step))
 
-  detected_parts = []
-  for engine, parts in scans.iteritems():
-    logging.info("%s found %d results" % (engine, len(parts)))
+    # Clean working directory
+    if os.path.isdir(dsplit_dir): shutil.rmtree(dsplit_dir)
+    # Generate parts
+    splitter.dsplit(args.file, todir=dsplit_dir, chunksize=step,
+                    offset=offset, limit=limit, fill=not args.truncate)
 
-    for part, result in parts.iteritems():
-      if result[0] == 'FOUND':
-        matched = parts_pattern.match(part)
-        part_n = int(matched.group(1))
-        detected_parts.append(part_n)
-        logging.info("Part %d detected as %s" % (part_n, result[1]))
+    logging.info("Beginning scanning at %s..." % (dsplit_dir))
+    scans = multi_av.scan(dsplit_dir, multiav.core.AV_SPEED_MEDIUM)
 
-    min_part = min(detected_parts)
-    logging.info("First detected part by %s is %d" % (engine, min_part))
-    offset = min_part * step
-    logging.info("Signature for %s starts at offset %d - %d" % (engine, offset, offset + step))
+    for engine, parts in scans.iteritems():
+      logging.info("%s found %d results" % (engine, len(parts)))
+
+      detected_parts = scan_parts(parts)
+      min_part = min(detected_parts)
+      logging.info("First detected part by %s is %d" % (engine, min_part))
+      offset = min_part * step
+      limit = step * 2
+      logging.info("Signature for %s starts at offset %d - %d, error: %d"
+                    % (engine, offset, offset + step, step))
+      # TODO: Support multiple engines
+      break   # Right now only one engine at a time
+      # /for
+
+    if i >= max_iter:
+      logging.warn("Reached maximum iterations (%d). Breaking..." % (max_iter))
+      break
+
+    step /= 2
+    i += 1
+    logging.info("------------------------------------------------------")
+    # /while
+
+  logging.info("Signature start located to offset %d - %d, error: %d"
+                % (offset, offset + step, step))
